a17b8a3c06325d22e22ad03f5b1a1cd7fd34035e
Enver <a.enver.com@gmail.com>
2018-02-18 23:02:58 +0200

    Project Setup Completed Successfully

---
+++ bookexchangebackend/src/main/java/net/enver/bookexchangebackend/App.java
@@ -0,0 +1,13 @@
+package net.enver.bookexchangebackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
